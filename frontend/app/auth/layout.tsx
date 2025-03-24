10f234a42428a705f18deb74f5b05fa4440474be
Nati <nathanmes124@gmail.com>
2025-03-24 14:38:14 +0300

    update layout

--- frontend/app/auth/layout.tsx
+++ frontend/app/auth/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return <div className="grid grid-cols-2">{children}</div>;
 };
 
 export default AuthLayout;
--- frontend/app/auth/signin/page.tsx
+++ frontend/app/auth/signin/page.tsx
@@ -2,10 +2,10 @@ import Signin from "@/components/auth/Signin";
 
 const page = () => {
   return (
-    <div className="grid grid-cols-2">
+    <>
       <Signin />
       <div className="">image</div>
-    </div>
+    </>
   );
 };
 
