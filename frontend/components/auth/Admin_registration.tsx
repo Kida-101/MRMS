4e897dbec37bd54a3962a2119673605084927d56
Nati <nathanmes124@gmail.com>
2025-03-25 19:20:50 +0300

    minor change on select input

--- frontend/components/auth/Admin_registration.tsx
+++ frontend/components/auth/Admin_registration.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import {
@@ -10,22 +10,21 @@ import {
 } from "@/components/ui/select";
 
 export default function Admin_registration() {
-  return (<div className="min-h-screen bg-gradient-to-br from-primary-50 to-primary-100 flex items-center justify-center bg-gray-100">
-        <div className="bg-white rounded-2xl shadow-xl p-8 space-y-6 w-150">
-          <div className="text-center space-y-2">
-            <h1 className="font-bold tracking-tight text-2xl">Admin Registration</h1>
-          </div>
-          <form className="space-y-4">
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-primary-50 to-primary-100 flex items-center justify-center bg-gray-100">
+      <div className="bg-white rounded-2xl shadow-xl p-8 space-y-6 w-150">
+        <div className="text-center space-y-2">
+          <h1 className="font-bold tracking-tight text-2xl">
+            Admin Registration
+          </h1>
+        </div>
+        <form className="space-y-4">
           <div className="flex space-x-4 gap-x-4">
             <div className="flex-1 space-y-2">
               <label htmlFor="text" className="text-sm font-semibold">
                 Full Name
               </label>
-              <Input
-                type="text"
-                id="text"
-                required
-              />
+              <Input type="text" id="text" required />
             </div>
             <div className="flex-1 space-y-2">
               <label htmlFor="role" className="text-sm font-semibold">
@@ -36,14 +35,14 @@ export default function Admin_registration() {
                   <SelectValue placeholder="Select an option" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="light">Admin</SelectItem>
-                  <SelectItem value="dark">Super Admin</SelectItem>
+                  <SelectItem value="admin">Admin</SelectItem>
+                  <SelectItem value="superadmin">Super Admin</SelectItem>
                 </SelectContent>
               </Select>
             </div>
           </div>
           <div className="flex space-x-4 gap-x-4">
-          <div className="flex-1 space-y-2">
+            <div className="flex-1 space-y-2">
               <label htmlFor="email" className="text-sm font-semibold">
                 Email
               </label>
@@ -51,35 +50,33 @@ export default function Admin_registration() {
                 type="email"
                 id="email"
                 placeholder="youremail@gmial.com"
-                required/>
+                required
+              />
             </div>
             <div className="flex-1 space-y-2 mt-9">
               {/* <label htmlFor="Email" className="text-sm font-semibold">Image
               </label> */}
-              </div>
             </div>
-            <div className="flex space-x-4 gap-x-4">
-              <div className="flex-1 space-y-2">
+          </div>
+          <div className="flex space-x-4 gap-x-4">
+            <div className="flex-1 space-y-2">
               <label htmlFor="tel" className="text-sm font-semibold">
                 Phone Number
               </label>
-              <Input
-                type="tel"
-                id="tel"
-                placeholder="+251 or +07"
-                required/>
-              </div>
-              <div className="flex-1 space-y-2 gap-x-4">
-                <label htmlFor="image" className="text-sm font-semibold">Image</label>
-                <Input
-                type="file"
-                accept='image/*'
-                />
-              </div>
+              <Input type="tel" id="tel" placeholder="+251 or +07" required />
             </div>
-            <Button className="w-full" type="submit">Save</Button>
-          </form>
-        </div>
+            <div className="flex-1 space-y-2 gap-x-4">
+              <label htmlFor="image" className="text-sm font-semibold">
+                Image
+              </label>
+              <Input type="file" accept="image/*" />
+            </div>
+          </div>
+          <Button className="w-full" type="submit">
+            Save
+          </Button>
+        </form>
       </div>
-      );
+    </div>
+  );
 }
