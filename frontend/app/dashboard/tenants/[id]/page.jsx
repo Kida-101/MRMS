9e44365158ec2e50eb6b35bf9a34b3cbff063ebb
Nati <nathanmes124@gmail.com>
2025-04-29 12:30:47 +0300

    ✨ feat: add single tenant profile page

---
+++ frontend/app/dashboard/tenants/[id]/page.jsx
@@ -0,0 +1,45 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import MoveBack from "@/components/ui/MoveBack";
+
+const page = () => {
+  return (
+    <div className="flex flex-col h-full gap-6">
+      <div className="flex items-center justify-between">
+        <div>
+          <MoveBack text="Back" />
+        </div>
+        <div>
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/dashboard/tenants">
+                  Tenants
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Detail</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </div>
+      <div className="flex-1">
+        <div className="border border-dashed rounded-md h-full relative p-4 flex flex-col gap-4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
