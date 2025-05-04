33532b3bb725a3337af65565b41a1fd4dbbb751a
Nati <nathanmes124@gmail.com>
2025-05-04 16:33:53 +0300

    ♻️ refactor: integrate backend for single tenant detail page

--- frontend/app/dashboard/tenants/[id]/page.jsx
+++ frontend/app/dashboard/tenants/[id]/page.jsx
@@ -1,6 +1,11 @@
 "use client";
 
 import MoveBack from "@/components/ui/MoveBack";
+import RoomCard from "@/components/dashboard/tenant/RoomCard";
+import BusinessCard from "@/components/dashboard/tenant/BusinessCard";
+import LeaseCard from "@/components/dashboard/tenant/LeaseCard";
+import EmergencyContactCard from "@/components/dashboard/tenant/EmergencyContactCard";
+import TenantInfoCard from "@/components/dashboard/tenant/TenantInfoCard";
 import Link from "next/link";
 import {
   Breadcrumb,
@@ -10,12 +15,23 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { useRouter } from "next/router";
+import { useParams } from "next/navigation";
+import { getTenant } from "@/lib/api/tenantsApi";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "@/components/ui/Loading";
 
 const page = () => {
-  // const router = useRouter();
+  const { id } = useParams();
 
-  // console.log(router);
+  const {
+    data: tenant,
+    isLoading: isLoadingTenant,
+    isError,
+  } = useQuery({
+    queryKey: ["tenant", id],
+    queryFn: () => getTenant(id),
+    enabled: !!id,
+  });
 
   return (
     <div className="flex flex-col h-full gap-6">
@@ -46,11 +62,20 @@ const page = () => {
         </div>
       </div>
       <div className="flex-1">
-        <div className="border border-dashed rounded-md h-full relative p-4 flex flex-col gap-4">
-          <div>Personal Info</div>
-          <div>Business Info</div>
-          <div>Emergency Contact Info</div>
-          <div>Lease Info</div>
+        <div className="border h-full border-dashed rounded-md relative p-4 gap-4 grid lg:grid-cols-3 md:grid-cols-2">
+          {isLoadingTenant ? (
+            <Loading />
+          ) : (
+            <>
+              <TenantInfoCard tenant={tenant} />
+              <EmergencyContactCard
+                emergencyContact={tenant.emergencyContact}
+              />
+              <BusinessCard businessInfo={tenant.businessInfo} />
+              <LeaseCard lease={tenant.leaseId} />
+              <RoomCard room={tenant.roomId} />
+            </>
+          )}
         </div>
       </div>
     </div>
